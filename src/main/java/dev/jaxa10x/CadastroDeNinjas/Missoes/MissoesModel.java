617fa641a1ef02f11da87a24186fc03b9352aed3
Leonardo Perdone <leonardo.perdone@gmail.com>
2025-07-28 20:22:16 -0300

    fix: Coloca a coluna de email como UNIQUE

--- src/main/java/dev/jaxa10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/jaxa10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.jaxa10x.CadastroDeNinjas.Missoes;
 
 import dev.jaxa10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
--- src/main/java/dev/jaxa10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/jaxa10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,18 +2,25 @@ package dev.jaxa10x.CadastroDeNinjas.Ninjas;
 
 import dev.jaxa10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 //Entity transforma uma classe em uma entidade do DB
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Column( unique = true) // essa coluna e unica sem se reptir o email
     private String email;
     private int idade;
 
@@ -21,36 +28,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") //Foreing key ou chave estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
