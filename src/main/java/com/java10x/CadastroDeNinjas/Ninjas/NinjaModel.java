2280709e35c16f2a14e91608fc9dcc828522ff10
Leonardo Perdone <leonardo.perdone@gmail.com>
2025-08-26 17:01:08 -0300

    Atualização Ninja model, controle, missoes

--- src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -10,13 +10,10 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    @Column(name = "nome")
-    String nome;
-    @Column(name = "email")
-    String email;
-    @Column(name = "idade")
-    int idade;
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
 
     public NinjaModel() {
     }
